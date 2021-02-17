0588b1bec07c4e3d8672272c93f2aa84019d0ead
cPango <ryan.beckett@outlook.com>
2021-02-17 18:41:09 +0000

    change shapes

--- Assets/scripts/TapToPlace.cs
+++ Assets/scripts/TapToPlace.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+
+enum Shapes : int
+{
+    Cube = 0,
+    Sphere = 1,
+}
 
 [RequireComponent(typeof(ARRaycastManager))]
 public class TapToPlace : MonoBehaviour
 {
+    private Shapes selectedShape;
+    public List<GameObject> objsToInst;
+    public GameObject spawnedObject;
 
-    public GameObject instantiateGameObj;
-    private GameObject spawnedObject;
     private ARRaycastManager _arRaycastManager;
     private Vector2 touchPos;
-
     static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    public GameObject testText;
 
     private void Awake()
     {
@@ -26,6 +34,7 @@ public class TapToPlace : MonoBehaviour
         if (Input.touchCount > 0)
         {
             touchPosition = Input.GetTouch(0).position;
+            Debug.Log(Input.GetTouch(0));
             return true;
         }
         touchPosition = default;
@@ -40,24 +49,46 @@ public class TapToPlace : MonoBehaviour
 
         if (_arRaycastManager.Raycast(touchPos, hits, TrackableType.PlaneWithinPolygon)) 
         {
-            var hitPose = hits[0].pose;
-
-            if (spawnedObject == null)
-            {
-                spawnedObject = Instantiate(instantiateGameObj, hitPose.position, hitPose.rotation);
-            }
-            else
-            {
-                spawnedObject.transform.position = hitPose.position;
-            }
+            PlaceMoveObj(hits[0].pose);
+        }
+        if (touchPos != null) 
+        {
+            testText.GetComponent<Text>().text = "X: " + touchPos.x + " | Y: " + touchPos.y;
         }
     }
 
-    private bool IsPointerOverUIObject() {
+    private void PlaceMoveObj(Pose hitPose) 
+    {
+        if (spawnedObject == null)
+        {
+            spawnedObject = Instantiate(objsToInst[(int)selectedShape], hitPose.position, hitPose.rotation);
+        }
+        else
+        {
+            spawnedObject.transform.position = hitPose.position;
+        }
+    }
+
+    private bool IsPointerOverUIObject() 
+    {
         PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
         eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         List<RaycastResult> results = new List<RaycastResult>();
         EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
         return results.Count > 0;
+    }
+
+    public void ChangeObjToCube() {
+        testText.GetComponent<Text>().text = "Cube Pressed";
+        Destroy(spawnedObject);
+        selectedShape = Shapes.Cube;
+        Debug.Log(objsToInst[(int)selectedShape]);
+    }
+
+    public void ChangeObjToSphere() {
+            testText.GetComponent<Text>().text = "Sphere Pressed";
+        Destroy(spawnedObject);
+        selectedShape = Shapes.Sphere;
+        Debug.Log(objsToInst[(int)selectedShape]);
     }
 }
